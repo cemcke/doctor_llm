d93029fb30397fce05874ad962d5df238b4228ed
cemcke <115202165+cemcke@users.noreply.github.com>
2024-01-11 15:22:13 +0800

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,5 +1,5 @@
 # 首先导入所需第三方库
-import codecs
+
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.document_loaders import UnstructuredMarkdownLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
